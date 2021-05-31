d045f7d675aae70bca640193b478f0cede2452cb
rodrigoestef <rodrigoestef@gmail.com>
2021-05-31 19:01:03 -0300

    FloatingButton em todas as paginas e loading fake para reloading dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -32,52 +32,64 @@ const double _fabDimension = 56.0;
 class _HomeState extends State<Home> {
   static const List<Widget> _page = <Widget>[Graphics(), TablePage()];
   int _indexPage = 0;
+  bool forceLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meus Gastos'),
-      ),
-      body: PageTransitionSwitcher(
-        transitionBuilder: (Widget child, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return FadeThroughTransition(
-            animation: animation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          );
-        },
-        child: _page[_indexPage],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _indexPage,
-        onTap: (int index) {
-          setState(() {
-            _indexPage = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.assessment), label: "Grafico"),
-          BottomNavigationBarItem(icon: Icon(Icons.reorder), label: "Tabela"),
-        ],
-      ),
-      floatingActionButton: _indexPage == 0
-          ? OpenContainer(
-              closedColor: Colors.green,
-              closedShape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(_fabDimension)),
-              ),
-              closedBuilder: (BuildContext c, VoidCallback _) => SizedBox(
-                height: _fabDimension,
-                width: _fabDimension,
-                child: Center(
-                  child: Icon(Icons.add, color: Colors.white),
-                ),
-              ),
-              openBuilder: (BuildContext c, VoidCallback _) => FormNewItem(),
-            )
-          : null,
-    );
+        appBar: AppBar(
+          title: Text('Meus Gastos'),
+        ),
+        body: PageTransitionSwitcher(
+          transitionBuilder: (Widget child, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return FadeThroughTransition(
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              child: child,
+            );
+          },
+          child: forceLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : _page[_indexPage],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indexPage,
+          onTap: (int index) {
+            setState(() {
+              _indexPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.assessment), label: "Grafico"),
+            BottomNavigationBarItem(icon: Icon(Icons.reorder), label: "Tabela"),
+          ],
+        ),
+        floatingActionButton: OpenContainer(
+          closedColor: Colors.green,
+          closedShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(_fabDimension)),
+          ),
+          closedBuilder: (BuildContext c, VoidCallback _) => SizedBox(
+            height: _fabDimension,
+            width: _fabDimension,
+            child: Center(
+              child: Icon(Icons.add, color: Colors.white),
+            ),
+          ),
+          openBuilder: (BuildContext c, VoidCallback _) => FormNewItem(),
+          onClosed: (_) {
+            setState(() {
+              forceLoading = true;
+            });
+            Future.delayed(Duration(seconds: 1)).then((_) {
+              setState(() {
+                forceLoading = false;
+              });
+            });
+          },
+        ));
   }
 }
